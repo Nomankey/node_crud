c903a4e8d21fdb82e216295462dab908e4b00a42
Shawn CK.P <snowy213@outlook.com>
2021-05-06 15:04:02 +0900

    installed babel and nodemon

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hi');
+import express from "express";
+
+// const express = require('express'); //getting another module from somewhere else // requiring express
+const app = express(); // making an application through a variable by executing express
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening localhost ${PORT}`)
+}
+
+const handleHome = (req, res) => {
+    res.send("Hello from starbucks")
+};
+
+const handleProfile = (req, res) => {
+    res.send("You are on my profile")
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
