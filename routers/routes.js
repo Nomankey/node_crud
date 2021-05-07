1d855bfb5868625effa95c169be62d92e9f54385
Shawn CK.P <snowy213@outlook.com>
2021-05-07 16:17:01 +0900

    added new file with routers in it

---
+++ routers/routes.js
@@ -0,0 +1,12 @@
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+//Users
+
+const USER = "/users";
+const USER_DETAIL = "/:id";
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
