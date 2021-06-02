5242a58b0c5e186fcf631398176eb88220b57b66
Shawn CK.P <snowy213@outlook.com>
2021-06-02 10:50:39 +0900

    used routers for the sever routes

--- src/server.js
+++ src/server.js
@@ -2,9 +2,35 @@ import express from "express";
 import morgan from "morgan";
 
 const PORT = 8888;
+
 const app = express();
 const logger = morgan("dev");
+app.use(logger); //logger function will return middleware
+// app.get('/', home);
+// app.get('/login', login);
 
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+
+
+
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const routerLogger = (req, res, next) => {
     console.log('PATH: ', req.path);
@@ -21,9 +47,6 @@ const home = (req, res) => {
 
 const login = (req, res) => res.send("login");
 
-app.use(logger); //logger function will return middleware
-app.get('/', home);
-app.get('/login', login);
 
 
 const handleListening = () => console.log(`server is listening on port http://localhost:${PORT}`);
