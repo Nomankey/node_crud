2bc6f9604493d6f3d5f051474c2a243630c0e650
Shawn CK.P <snowy213@outlook.com>
2021-06-07 20:24:57 +0900

    edit video form

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,7 +45,27 @@ export const postEdit = (req, res) => {
     const id = req.params.id;
     const title = req.body.title;
     videos[id - 1].title = title;
-    return res.redirect(`/videos/${id}`);
+    res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+    res.render("upload", { pageTitle: "Upload Video" });
+  };
+  
+export const postUpload = (req, res) => {
+// here we will add a video to the videos array.
+    const title = req.body.title
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createdAt: "just now",
+        views: 0,
+        id: videos.length + 1,
+    };
+    videos.push(newVideo);
+    res.redirect("/");
+};
+
 
 export const search = (req, res) => res.send("Search");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { watch, getEdit, postEdit, upload, deleteVideo } from "../controllers/videoController";
+import { watch, getEdit, getUpload, postEdit, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch); //by using regex it can only allow digits from now on
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
