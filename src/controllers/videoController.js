a720ec3331bcfecf6c2d6158206722f2729494a9
Shawn CK.P <snowy213@outlook.com>
2021-06-14 17:11:20 +0900

    schema

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,14 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-    console.log("I Start");
-    Video.find({}, (error, videos) => {
-        return res.render("home", { pageTitle: "Home", videos });
-});
-  console.log("I finish first");
+// Video.find({}, (error, videos) => {
+//     return res.render("home", { pageTitle: "Home", videos });
+// });
+
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
     const { id } = req.params;
     return res.render("watch", { pageTitle: `Watching` });
@@ -23,7 +25,19 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-    const { title } = req.body;
-    return res.redirect("/");
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    try {
+      await Video.create({
+        title,
+        description,
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+      });
+      return res.redirect("/");
+    } catch (error) {
+      return res.render("upload", {
+        pageTitle: "Upload Video",
+        errorMessage: error._message,
+      });
+    }
 };
